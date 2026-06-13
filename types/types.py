9d9a7b0060e60134ffe3625fda6061500c11e45b
Mariusz Janosz <mariuszjanosz10@gmail.com>
2026-06-13 19:14:50 +0200

    Python built-in datatypes

---
+++ types/types.py
@@ -0,0 +1,31 @@
+var_int = 5
+print(type(var_int))
+var_float = 5.6
+print(type(var_float))
+var_complex = 3 + 2j
+print(type(var_complex))
+var_str = "string"
+print(type(var_str))
+var_list = []
+print(type(var_list))
+var_tuple = ()
+print(type(var_tuple))
+var_range = range(10)
+print(type(var_range))
+var_dict = {"key" : "value"}
+print(type(var_dict))
+var_set = {"member"}
+print(type(var_set))
+var_frozenset = frozenset({"member"})
+print(type(var_frozenset))
+var_none = None
+print(type(var_none))
+var_bool = True
+print(type(var_bool))
+var_bytes = b"bytes"
+print(type(var_bytes))
+var_bytearray = bytearray(5)
+print(type(var_bytearray))
+var_memoryview = memoryview(bytes(10))
+print(var_memoryview)
+
